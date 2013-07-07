ceb260ae763b1baa776ca3cd854f88f7d25f8b72
Robin Lauren <aws@llauren.net>
2013-07-07 22:44:28 +0000

    Changed web.js to read included file instead

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = new Buffer(42);
+
+buf = fs.readFileSync( 'hello.html' );
+html = buf.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
